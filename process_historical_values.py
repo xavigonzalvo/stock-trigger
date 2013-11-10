98f944c3db4a18d5a90d38abba02308d399f39e5
Xavi <xavigonzalvo@gmail.com>
2013-11-10 11:44:52 +0000

    Clean main in historical data processor.

--- process_historical_values.py
+++ process_historical_values.py
@@ -19,25 +19,30 @@ flags.FLAGS.add_argument("--num_weeks", type=int, default=-1,
 FLAGS = flags.Parse()
 
 
-def main():
-    # Read data.
-    data = weeks_processor.ReadData(FLAGS.filename)
-    total_num_weeks = len(data)
-    print '%d weeks for analysis (%d months, %d years)' % (
-        total_num_weeks, total_num_weeks / 4, total_num_weeks / 4 / 12)
+def MakePlots(rev_week_values, slopes, fitter,
+              poly_quadratic, poly_cubic, poly_linear):
+    plt.subplot(511)
+    plt.title('Histogram of gradients') 
+    util.PlotHistogram(slopes)
 
-    # Process data.
-    runner = weeks_processor.WeeksProcessor(data, FLAGS.num_weeks)
-    (slopes, week_values, mean, std) = runner.Process()
-    rev_week_values = week_values[::-1]
+    plt.subplot(512)
+    plt.title('Value per week')
+    plt.plot(rev_week_values)
 
-    result = week_result_pb2.WeekResult()
-    result.mean = mean
-    result.std = std
+    plt.subplot(513)
+    plt.title('Quadratic fit')
+    fitter.PlotPolynomial(poly_quadratic)
 
-    # Fit model.
-    fitter = curve_fitting.CurveFitting(rev_week_values)
+    plt.subplot(514)
+    plt.title('Cubic fit')
+    fitter.PlotPolynomial(poly_cubic)
 
+    plt.subplot(515)
+    plt.title('Linear fit')
+    fitter.PlotPolynomial(poly_linear)
+
+
+def UpdateProto(fitter, result):
     poly_linear, error = fitter.Linear()
     linear_poly = result.poly.add()
     linear_poly.order = 1
@@ -56,29 +61,36 @@ def main():
     cubic_poly.order = 3
     cubic_poly.coef.extend(list(poly_cubic))
     cubic_poly.error = error
+    return (poly_quadratic, poly_cubic, poly_linear)
 
-    # Plots.
-    plt.subplot(511)
-    plt.title('Histogram of gradients') 
-    util.PlotHistogram(slopes)
 
-    plt.subplot(512)
-    plt.title('Value per week') 
-    plt.plot(rev_week_values)
+def main():
+    # Read data.
+    data = weeks_processor.ReadData(FLAGS.filename)
+    total_num_weeks = len(data)
+    print '%d weeks for analysis (%d months, %d years)' % (
+        total_num_weeks, total_num_weeks / 4, total_num_weeks / 4 / 12)
 
-    plt.subplot(513)
-    plt.title('Quadratic fit')
-    fitter.PlotPolynomial(poly_quadratic)
+    # Process data.
+    runner = weeks_processor.WeeksProcessor(data, FLAGS.num_weeks)
+    (slopes, week_values, mean, std) = runner.Process()
 
-    plt.subplot(514)
-    plt.title('Cubic fit')
-    fitter.PlotPolynomial(poly_cubic)
+    result = week_result_pb2.WeekResult()
+    result.mean = mean
+    result.std = std
 
-    plt.subplot(515)
-    plt.title('Linear fit')
-    fitter.PlotPolynomial(poly_linear)
+    # Fit model.
+    rev_week_values = week_values[::-1]
+    fitter = curve_fitting.CurveFitting(rev_week_values)
 
-    # Save.
+    # Update results.
+    (poly_quadratic, poly_cubic, poly_linear) = UpdateProto(fitter, result)
+
+    # Prepare plots.
+    MakePlots(rev_week_values, slopes, fitter,
+              poly_quadratic, poly_cubic, poly_linear)
+
+    # Save plots.
     filename = '%s-%s' % (
         util.Basename(FLAGS.filename),
         str(FLAGS.num_weeks) if FLAGS.num_weeks > 0 else 'all')
