50b2bb96dc383f8e9d24e5965a9b013c02482582
Xavi <xavigonzalvo@gmail.com>
2014-02-08 11:27:26 +0000

    Refactor report generator

--- ndb_data.py
+++ ndb_data.py
@@ -4,14 +4,23 @@ from google.appengine.ext import ndb
 
 
 class ReportProperty(ndb.Model):
+    # Date when symbols were processed. When generating a report,
+    # this is the date referring to when the data of the symbols
+    # was generated. 
     last = ndb.DateProperty()
 
 
 class AnalysisProperty(ndb.Model):
+    # When the data was saved for this analysis.
     date = ndb.DateProperty()
-    data = ndb.BlobProperty()  # serialized protobuf
+
+    # serialized protobuf
+    data = ndb.BlobProperty()
 
 
 class SymbolProperty(ndb.Model):
+    # Name of the symbol (eg. BPI.L).
     name = ndb.StringProperty()
+
+    # A list of analysis data.
     analysis = ndb.StructuredProperty(AnalysisProperty, repeated=True)
--- weekly_best_stocks_report.py
+++ weekly_best_stocks_report.py
@@ -15,61 +15,76 @@ import week_result_pb2
 import webapp2
 
 
-def CreateReport(hard_data_filter, medium_data_filter):
-    report = ndb_data.ReportProperty.query().get()
-    symbols = ndb_data.SymbolProperty.query()
-    count_correct_analysis = 0
-    count_total = 0
-    hard_good_symbols = []
-    hard_good_symbols_html = []
-    medium_good_symbols = []
-    medium_good_symbols_html = []
-    for symbol in symbols:
-        for analysis in symbol.analysis:
-            if analysis.date != report.last:
-                continue            
-            count_correct_analysis += 1
-            data = week_result_pb2.WeekResult()
-            data.ParseFromString(analysis.data)
-            
-            if not filter_utils.Filter(data, hard_data_filter):
-                hard_good_symbols.append(data.name)
-                url_symbol = urllib.quote('LON:%s' % data.name.replace('.L',''))
-                hard_good_symbols_html.append(
-                    '<a href="%s?q=%s&ei=HejzUoiYBavGwAPk8gE">%s</a><br>' % (
-                        gae_config.WEB_FINANCE, url_symbol, data.name))
-            if not filter_utils.Filter(data, medium_data_filter):
-                medium_good_symbols.append(data.name)
-                url_symbol = urllib.quote('LON:%s' % data.name.replace('.L',''))
-                medium_good_symbols_html.append(
-                    '<a href="%s?q=%s&ei=HejzUoiYBavGwAPk8gE">%s</a><br>' % (
-                        gae_config.WEB_FINANCE, url_symbol, data.name))
-            break
-        count_total += 1
+class SymbolProcessor(object):
+    """Class to handle symbols, filter and generating reports."""
 
-    msg = ['report last: %s' % report.last,
-           'count_total = %d' % count_total,
-           'count_correct_analysis = %d' % count_correct_analysis,
-           ] + hard_good_symbols_html
-    hard_msg_symbols_html = ['%s<br>' % symbol for symbol in hard_good_symbols_html]
-    medium_msg_symbols_html = ['%s<br>' % symbol for symbol in medium_good_symbols_html]
-    msg_html = """<h2>Stats</h2>
-                  <p>Date: %s<br>
-                  count_total = %d<br>
-                  count_correct_analysis = %d</p>
-                  <h2>Hard filtered symbols</h2><p>%s</p>
-                  <h2>Medium filtered symbols</h2><p>%s</p>""" % (
-        report.last,
-        count_total, count_correct_analysis, "".join(hard_msg_symbols_html),
-        "".join(medium_msg_symbols_html))    
-    return '\n'.join(msg), msg_html
+    def __init__(self):
+        self.good_symbols = {}
+        self.good_symbols['hard'] = []
+        self.good_symbols['medium'] = []
+        self.report_date = None
+        self.count_correct_analysis = 0
+        self.count_total = 0
+
+    def FilterSymbols(self, hard_data_filter, medium_data_filter):
+        """Filters symbols using filters. Uses the last date stored in
+        report as the reference to pick values from stored symbols"""
+        report = ndb_data.ReportProperty.query().get()
+        self.report_date = report.last
+
+        symbols = ndb_data.SymbolProperty.query()
+        self.count_correct_analysis = 0
+        self.count_total = 0
+        for symbol in symbols:
+            for analysis in symbol.analysis:
+                if analysis.date != self.report_date:
+                    continue            
+                self.count_correct_analysis += 1
+                data = week_result_pb2.WeekResult()
+                data.ParseFromString(analysis.data)
+                
+                if not filter_utils.Filter(data, hard_data_filter):
+                    self.good_symbols['hard'].append(data.name)
+                if not filter_utils.Filter(data, medium_data_filter):
+                    self.good_symbols['medium'].append(data.name)
+                break
+            self.count_total += 1
+
+    def _GenerateSymbolReport(self, symbols):
+        """Returns a list of HTML lines with a link to each symbol."""
+        html_lines = []
+        _HTML_PART = '<a href="%s?q=%s&ei=HejzUoiYBavGwAPk8gE">%s</a><br>'
+        for symbol in symbols:
+            url_symbol = urllib.quote('LON:%s' % symbol.replace('.L',''))
+            html_lines.append(_HTML_PART % (gae_config.WEB_FINANCE, url_symbol,
+                                            symbol))
+        return html_lines
+
+    def CreateReport(self):
+        """Generates an HTML report given good symbols."""
+        hard_part = self._GenerateSymbolReport(self.good_symbols['hard'])
+        medium_part = self._GenerateSymbolReport(self.good_symbols['medium'])            
+    
+        hard_msg_symbols_html = ['%s<br>' % symbol for symbol in hard_part]
+        medium_msg_symbols_html = ['%s<br>' % symbol for symbol in medium_part]
+        msg_html = """<h2>Stats</h2>
+                      <p>Date: %s<br>
+                      count_total = %d<br>
+                      count_correct_analysis = %d</p>
+                      <h2>Hard filtered symbols</h2><p>%s</p>
+                      <h2>Medium filtered symbols</h2><p>%s</p>""" % (
+            self.report_date,
+            self.count_total,
+            self.count_correct_analysis,
+            "".join(hard_msg_symbols_html),
+            "".join(medium_msg_symbols_html))    
+        return msg_html
 
 
-def SendReport(report, report_html):
+def SendReport(report_html):
     message = mail.EmailMessage(sender=gae_config.SENDER_MAIL,
                                 to=gae_config.MAILS,
                                 subject=gae_config.WEEKLY_REPORT_SUBJECT)
-    message.body = report
     message.html = report_html
     message.send()
 
@@ -84,9 +99,12 @@ class BestStocksReport(webapp2.RequestHandler):
         gae_utils.ReadTextProto('filters/hard.ascii_proto', hard_data_filter)
         medium_data_filter = filter_pb2.Filter()
         gae_utils.ReadTextProto('filters/medium.ascii_proto', medium_data_filter)
+        
+        processor = SymbolProcessor()
+        processor.FilterSymbols(hard_data_filter, medium_data_filter)
+        report_html = processor.CreateReport()
+        SendReport(report_html)
 
-        report, report_html = CreateReport(hard_data_filter, medium_data_filter)
-        SendReport(report, report_html)
         self.response.write(report_html)
         self.response.write('<br>Finished')
 
