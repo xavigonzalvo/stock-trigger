e72dce553a49219d96ac9b489094d6f37bb82039
Xavi <xavigonzalvo@gmail.com>
2015-04-11 10:12:21 +0100

    Move AppEngine to gae folder

--- gae_config.py
+++
@@ -1,6 +0,0 @@
-"""Defines all configuration variables for the deep-trading system."""
-
-SENDER_MAIL = 'xavigonzalvo@gmail.com'
-MAILS = ['xavigonzalvo@gmail.com, ele.barquero@gmail.com']
-WEEKLY_REPORT_SUBJECT = 'Weekly report'
-WEB_FINANCE = 'https://www.google.co.uk/finance'
