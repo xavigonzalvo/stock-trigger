5a1885c243590a8e6174a2ac1276ed1574be7714
Xavi <xavigonzalvo@gmail.com>
2019-06-17 19:57:51 -0400

    Add gae_config

---
+++ gae/gae_config.py
@@ -0,0 +1,8 @@
+"""Defines all configuration variables for the deep-trading system."""
+
+SENDER_MAIL = 'xavigonzalvo@gmail.com'
+MAILS = ['xavigonzalvo@gmail.com']
+WEEKLY_REPORT_SUBJECT = 'Weekly report'
+WEB_FINANCE = 'https://www.google.co.uk/finance'
+ALPHA_ADVANTAGE_API = ''
+IEXCLOUD_TOKEN = ''
