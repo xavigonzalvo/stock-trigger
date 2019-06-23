87a742dcf4c59f43099e0b6799e6cab14c8b673c
Xavi <xavigonzalvo@gmail.com>
2019-06-23 08:46:13 -0400

    Add base reports accessor. Remove protos

---
+++ gae/base_reports_accessor.py
@@ -0,0 +1,18 @@
+import abc
+
+
+class BaseReportsAccessor(object):
+  """Base class to handle the reports container."""
+
+  __metaclass__ = abc.ABCMeta
+
+  def __init__(self):
+    self._reports = None
+
+  @abc.abstractmethod
+  def add_new_report(self, date):
+    raise NotImplementedError
+
+  @abc.abstractmethod
+  def last_date(self):
+    raise NotImplementedError
--- gae/gae_config.py
+++ gae/gae_config.py
@@ -1,8 +1,10 @@
 """Defines all configuration variables for the deep-trading system."""
 
+
 SENDER_MAIL = 'xavigonzalvo@gmail.com'
 MAILS = ['xavigonzalvo@gmail.com']
 WEEKLY_REPORT_SUBJECT = 'Weekly report'
 WEB_FINANCE = 'https://www.google.co.uk/finance'
 ALPHA_ADVANTAGE_API = ''
 IEXCLOUD_TOKEN = ''
+FINANCE_FETCHER = 'iexcloud'
